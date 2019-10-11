f5db4dff605ee699b3be164ca3021d7c59839407
Leon Anavi <leon@anavi.org>
2019-10-12 00:00:45 +0300

    index.js: Check if file exists
    Make the script more user friendly by adding a check whether
    the specified sound file exists. This way on error, a human
    readable error message will be printed on the screen instead
    of unhandled ENOENT exception.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ var fs = require('fs');
 var lame = require('lame');
 var Speaker = require('speaker');
 
-fs.createReadStream(process.argv[2])
+// Check if the specified sound file exists
+var sound = process.argv[2];
+if (!fs.existsSync(sound)) {
+  console.log('File: ' + sound + ' not found.');
+  process.exit(1);
+}
+
+fs.createReadStream(sound)
   .pipe(new lame.Decoder())
   .on('format', function (format) {
     this.pipe(new Speaker(format));
